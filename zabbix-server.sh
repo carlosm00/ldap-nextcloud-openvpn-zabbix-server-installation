2937a9c09a7010c0bb12fc96feba2bfe5679cc6c
carlosm00 <50986837+carlosm00@users.noreply.github.com>
2020-06-07 17:49:37 +0200

    Update zabbix-server.sh

--- zabbix-server.sh
+++ zabbix-server.sh
@@ -19,8 +19,9 @@
 #
 # logs
 PROGNAME=$(basename $0)
-echo "${PROGNAME}:" >success_zabbix.log
-echo "${PROGNAME}:" >error_zabbix.log
+mkdir /tmp/zabbix_installation
+echo "${PROGNAME}:" >/tmp/zabbix_installation/success_ldap.log
+echo "${PROGNAME}:" >/tmp/zabbix_installation/error_ldap.log
 #
 #############################################################
 #
@@ -33,20 +34,20 @@ echo -n "[EN] Write your db user password / [ES] Escriba la contraseña del usua
 read DB_PS
 echo "create user "$DB_USER" password '"$DB_PS"';" > psql_db_zabbix.sql
 echo "create database zabbix;" >> psql_db_zabbix.sql
-chmod 770 psql_db_zabbix.sql 1>>success_zabbix.log 2>>error_zabbix.log
-chown postgres:postgres psql_db_zabbix.sql 1>>success_zabbix.log 2>>error_zabbix.log
-sudo -u postgres psql -f psql_db_zabbix.sql 1>>success_zabbix.log 2>>error_zabbix.log
+chmod 770 psql_db_zabbix.sql 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+chown postgres:postgres psql_db_zabbix.sql 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+sudo -u postgres psql -f psql_db_zabbix.sql 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
 if [ $? == 0 ]
 then
     echo "[EN] Databse created / [ES] Base de datos creada "
 else
     echo "[EN] Could not create databse / [ES] No se pudo crear la base de datos"
 fi
-rm psql_db_zabbix.sql 1>>success_zabbix.log 2>>error_zabbix.log
+rm psql_db_zabbix.sql 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
 # [EN] Zabbix schema on postgresql databse / [ES] Esquema de Zabbix en la base de datos de postgresql
 zcat /usr/share/doc/zabbix-server-pgsql/create.sql.gz | sudo -u zabbix psql zabbix 
 # [EN] Backup for zabbix server config file / [ES] Copia de seguridad del archivo ed configuración del servidor de zabbix
-cp /etc/zabbix/zabbix_server.conf /etc/zabbix/zabbix_server-.conf 1>>success_zabbix.log 2>>error_zabbix.log
+cp /etc/zabbix/zabbix_server.conf /etc/zabbix/zabbix_server-.conf 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
 # [EN] / [ES]
 cat << EOL > /etc/zabbix/zabbix_server.conf
 LogFile=/var/log/zabbix/zabbix_server.log
@@ -70,7 +71,7 @@ DB_PS=0
 # [EN] Redirection to zabbix home page / [ES] Redirección a la página principal de zabbix
 echo -n "[EN] Write your domain / [Es] Escriba su dominio : "
 read DOM
-echo $DOM | grep "." >>success_zabbix.log
+echo $DOM | grep "." >>/tmp/zabbix_installation/success_zabbix.log
 if [ $? != 0 ]
 then
     echo "[EN] Your domain name must contain at least a dot / [ES] Su dominio debe contener mínimo un punto "
@@ -83,27 +84,27 @@ cat << EOL > /var/www/html/index.html
 <html>
    <head>
       <title> ZABBIX </title>
-      <meta http-equiv = "refresh" content = "0; url = http://www.$DOM/zabbix" />
+      <meta http-equiv = "refresh" content = "0; url = http://$DOM/zabbix" />
    </head>
    <body>
    </body>
 </html>
 EOL
 # [EN] Service restart / [ES] Reinicio de los servicios
-service zabbix-server start 1>>success_zabbix.log 2>>error_zabbix.log
-update-rc.d zabbix-server enable 1>>success_zabbix.log 2>>error_zabbix.log
+service zabbix-server start 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+update-rc.d zabbix-server enable 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
 # [EN] Timezone for zabbix frontend / [ES] Zona Temporal en el frontend de zabbix
 echo "php_value date.timezone Europe/Madrid" >> /etc/zabbix/apache.conf
-service apache2 restart 1>>success_zabbix.log 2>>error_zabbix.log
+service apache2 restart 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
 # HTTPS
 echo -e -n "[EN] Do you want to enable HTTPS? / [ES] ¿Desea habilitar HTTPS? \n[Y/n]: "
 read
 if [[ $REPLY == "y" ]] || [[ $REPLY == "Y" ]]
 then
-    apt -qq -y install software-properties-common 1>>success_zabbix.log 2>>error_zabbix.log
-    apt -qq update 1>>success_zabbix.log 2>>error_zabbix.log
-    add-apt-repository ppa:certbot/certbot -y 1>>success_zabbix.log 2>>error_zabbix.log
-    apt -qq install python-certbot-apache -y 1>>success_zabbix.log 2>>error_zabbix.log
+    apt -qq -y install software-properties-common 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+    apt -qq update 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+    add-apt-repository ppa:certbot/certbot -y 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+    apt -qq install python-certbot-apache -y 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
     certbot --apache -d $DOM
 fi
 # [EN] Comprobation / [ES] Comprobación
@@ -135,35 +136,35 @@ then
     read DZ
     if [[ ! -z $( echo $DZ | grep "1") ]]
     then
-        mkdir -p /tmp/zabbix 1>>success_zabbix.log 2>>error_zabbix.log
-        cd /tmp/zabbix 1>>success_zabbix.log 2>>error_zabbix.log
-        wget -q https://raw.githubusercontent.com/y-u-r/nextcloud-zabbix/master/zbx_export_templates.xml 1>>success_zabbix.log 2>>error_zabbix.log
-        chmod 775 zbx_export_templates.xml 1>>success_zabbix.log 2>>error_zabbix.log
+        mkdir -p /tmp/zabbix 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+        cd /tmp/zabbix 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+        wget -q https://raw.githubusercontent.com/y-u-r/nextcloud-zabbix/master/zbx_export_templates.xml 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+        chmod 775 zbx_export_templates.xml 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
     fi
     if [[ ! -z $( echo $DZ | grep "2") ]]
     then
         mkdir -p /tmp/zabbix 1>>success_zabbix.log 2>>error_zabbix.log
         cd /tmp/zabbix 1>>success_zabbix.log 2>>error_zabbix.log
-        wget -q https://raw.githubusercontent.com/MrCirca/OpenLDAP-Cluster-Zabbix/master/zabbix_openldap_template.xml 1>>success_zabbix.log 2>>error_zabbix.log
-        wget -q https://raw.githubusercontent.com/MrCirca/OpenLDAP-Cluster-Zabbix/master/zabbix_openldap_value_mapping.xml 1>>success_zabbix.log 2>>error_zabbix.log
-        chmod 775 zabbix_openldap_template.xml zabbix_openldap_value_mapping.xml 1>>success_zabbix.log 2>>error_zabbix.log
+        wget -q https://raw.githubusercontent.com/MrCirca/OpenLDAP-Cluster-Zabbix/master/zabbix_openldap_template.xml 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+        wget -q https://raw.githubusercontent.com/MrCirca/OpenLDAP-Cluster-Zabbix/master/zabbix_openldap_value_mapping.xml 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+        chmod 775 zabbix_openldap_template.xml zabbix_openldap_value_mapping.xml 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
     fi
     if [[ ! -z $( echo $DZ | grep "3") ]]
     then
-        mkdir -p /tmp/zabbix 1>>success_zabbix.log 2>>error_zabbix.log
-        cd /tmp/zabbix 1>>success_zabbix.log 2>>error_zabbix.log
-        wget -q https://raw.githubusercontent.com/Grifagor/zabbix-openvpn/master/openvpn.xml 1>>success_zabbix.log 2>>error_zabbix.log
-        chmod 775 openvpn.xml 1>>success_zabbix.log 2>>error_zabbix.log
+        mkdir -p /tmp/zabbix 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+        cd /tmp/zabbix 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+        wget -q https://raw.githubusercontent.com/Grifagor/zabbix-openvpn/master/openvpn.xml 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+        chmod 775 openvpn.xml 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
     fi
     if [ $DZ == "a" ]
     then
-        mkdir -p /tmp/zabbix 1>>success_zabbix.log 2>>error_zabbix.log
-        cd /tmp/zabbix 1>>success_zabbix.log 2>>error_zabbix.log
-        wget -q https://raw.githubusercontent.com/y-u-r/nextcloud-zabbix/master/zbx_export_templates.xml 1>>success_zabbix.log 2>>error_zabbix.log
-        wget -q https://raw.githubusercontent.com/MrCirca/OpenLDAP-Cluster-Zabbix/master/zabbix_openldap_template.xml 1>>success_zabbix.log 2>>error_zabbix.log
-        wget -q https://raw.githubusercontent.com/MrCirca/OpenLDAP-Cluster-Zabbix/master/zabbix_openldap_value_mapping.xml 1>>success_zabbix.log 2>>error_zabbix.log
-        wget -q https://raw.githubusercontent.com/Grifagor/zabbix-openvpn/master/openvpn.xml 1>>success_zabbix.log 2>>error_zabbix.log
-        chmod 775 *.xml 1>>success_zabbix.log 2>>error_zabbix.log
+        mkdir -p /tmp/zabbix 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+        cd /tmp/zabbix 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+        wget -q https://raw.githubusercontent.com/y-u-r/nextcloud-zabbix/master/zbx_export_templates.xml 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+        wget -q https://raw.githubusercontent.com/MrCirca/OpenLDAP-Cluster-Zabbix/master/zabbix_openldap_template.xml 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+        wget -q https://raw.githubusercontent.com/MrCirca/OpenLDAP-Cluster-Zabbix/master/zabbix_openldap_value_mapping.xml 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+        wget -q https://raw.githubusercontent.com/Grifagor/zabbix-openvpn/master/openvpn.xml 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+        chmod 775 *.xml 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
     fi
 fi
 }
@@ -180,12 +181,12 @@ OSV=`grep VERSION_CODENAME /etc/os-release | cut -c18-`
 # 2. apt / dnf
 if [[ $OS == *"buntu"* ]] || [[ $OS == *"ebian"* ]]
 then
-    apt -qq update 1>>success_zabbix.log 2>>error_zabbix.log
-    apt -qq install -y apache2 postgresql php php-mbstring php-gd php-xml php-bcmath php-ldap php-pgsql 1>>success_zabbix.log 2>>error_zabbix.log
-    wget -q https://repo.zabbix.com/zabbix/4.0/$OSI/pool/main/z/zabbix-release/zabbix-release_4.0-3+${OSV}_all.deb 1>>success_zabbix.log 2>>error_zabbix.log
-    dpkg -i zabbix-release_4.0-3+${OSV}_all.deb 1>>success_zabbix.log 2>>error_zabbix.log
-    apt update 1>>success_zabbix.log 2>>error_zabbix.log
-    apt install zabbix-server-pgsql zabbix-frontend-php zabbix-agent -y 1>>success_zabbix.log 2>>error_zabbix.log
+    apt -qq update 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+    apt -qq install -y apache2 postgresql php php-mbstring php-gd php-xml php-bcmath php-ldap php-pgsql 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+    wget -q https://repo.zabbix.com/zabbix/4.0/$OSI/pool/main/z/zabbix-release/zabbix-release_4.0-3+${OSV}_all.deb 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+    dpkg -i zabbix-release_4.0-3+${OSV}_all.deb 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+    apt update 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
+    apt install zabbix-server-pgsql zabbix-frontend-php zabbix-agent -y 1>>/tmp/zabbix_installation/success_zabbix.log 2>>/tmp/zabbix_installation/error_zabbix.log
 # [EN] Execution function for 3 / [ES] Ejecución de función para 3
     conf
 # [EN] Execution function for 4 / [ES] Ejecución de función para 4
